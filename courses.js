dc188d02cdc332271c956d0dc1b9e757bb1c99ca
Ryan Cornett <ryandcornett@gmail.com>
2023-04-11 15:41:13 -0400

    Converted modals to dialog element, dynamically created course cards, and updated schedule.

--- courses.js
+++ courses.js
@@ -1,200 +1,151 @@
-import {coursesList, daysList, timesList, linksList, description, phoneNumbers, courseNameElement, daysElement, timesElement, syllabusElement, zoomElement, descriptionElement, instructorElement, emailAddressElement, phoneNumberElement} from './info.js'
-
-const courses = [
-    {name: coursesList[0],
-    days: `${daysList[0]} & ${daysList[1]}`,
-    time: timesList[0],
-    syllabus: linksList[1].sizemore,
-    zoom: linksList[0].laurel,
-    description: description.numberSense,
+let mathCourses = [
+    {name: "Number Sense",
+    days: "Monday & Tuesday",
+    time: "9:00-10:00",
+    syllabus: "https://docs.google.com/document/d/1lBnj1anz5cOa1L5bbV-CUbs2pysiH7Ys/edit#heading=h.gjdgxs",
+    zoom: "https://us02web.zoom.us/j/6447967863?pwd=aldCSW50bGlPVzhHMi9oczR4QUJUQT09",
+    description: "In this class, you'll sharpen your skills with operations, fractions, decimals, percents, and word problems. You'll also work with reading bar graphs, circle graphs, line plots, tables, and other ways of representing data.",
+    picture: "img/marks.jpg",
     instructor: "Mark Sizemore",
     email: "mark@laureladulted.org",
-    phone: phoneNumbers.london
+    phone: "(606) 878-9134"
     },
 
-    {name: coursesList[0],
-    days: `${daysList[2]} & ${daysList[3]}`,
-    time: timesList[0],
-    syllabus: linksList[1].ledford,
-    zoom: linksList[0].corbin,
-    description: description.numberSense,
-    instructor: "Jennifer Ledford",
-    email: "jennifer@laureladulted.org",
-    phone: phoneNumbers.corbin
-    },
-
-    {name: coursesList[1],
-    days: `${daysList[2]} & ${daysList[3]}`,
-    time: timesList[1],
-    syllabus: linksList[1].chesnut,
-    zoom: linksList[0].laurel,
-    description: description.geometry,
-    instructor: "Rachael Chesnut",
-    email: "rachael@laureladulted.org",
-    phone: phoneNumbers.london
-    },
-    
-    {name: coursesList[1],
-    days: daysList[1],
-    time: timesList[4],
-    syllabus: linksList[1].mosesGeo,
-    zoom: linksList[0].corbin,
-    description: description.geometry,
-    instructor: "Erin Moses",
-    email: "erin@laureladulted.org",
-    phone: phoneNumbers.corbin
-    },
-    
-    {name: coursesList[2],
-    days: daysList[0],
-    time: timesList[1],
-    syllabus: linksList[1].mosesAlg,
-    zoom: linksList[0].corbin,
-    description: description.algebra,
-    instructor: "Erin Moses",
-    email: "erin@laureladulted.org",
-    phone: phoneNumbers.corbin
-    },
-    
-    {name: coursesList[2],
-    days: daysList[1],
-    time: timesList[1],
-    syllabus: linksList[1].cornettAlg,
-    zoom: linksList[0].clay,
-    description: description.algebra,
-    instructor: "Ryan Cornett",
-    email: "ryan@laureladulted.org",
-    phone: phoneNumbers.clay
-    },
-    
-    {name: coursesList[2],
-    days: daysList[1],
-    time: timesList[6],
-    syllabus: linksList[1].lipps,
-    zoom: linksList[0].clay,
-    description: description.algebra,
+    {name: "Algebra",
+    days: "Tuesday",
+    time: "5:00-6:30",
+    syllabus: "https://docs.google.com/document/d/1WYkF4sSxJ9EgesBxfhVLGHaJ3iD9ZdIB/edit?usp=share_link&ouid=104017723111487459394&rtpof=true&sd=true",
+    zoom: "https://us02web.zoom.us/j/3619478526?pwd=WkJZUUUzdUx3dVp5RVdoV1h0amF0QT09",
+    description: "In this class, you'll work on expressions, equations, inequalities, functions, and graphs.",
+    picture: "img/rodneyl.jpg",
     instructor: "Rodney Lipps",
     email: "rodney@laureladulted.org",
-    phone: phoneNumbers.clay
+    phone: "(606) 599-1230"
+    },
+
+    {name: "Number Sense",
+    days: "Wednesday",
+    time: "10:30-12:00",
+    syllabus: "https://docs.google.com/document/d/1sNDHvD5xIXkQ_tqmWdguvl1L9KnBTL5y",
+    zoom: "https://us02web.zoom.us/j/4974845563?pwd=Q1ErU2gzaW9oNiszYmkyZXRVajRSUT09",
+    description: "In this class, you'll sharpen your skills with operations, fractions, decimals, percents, and word problems. You'll also work with reading bar graphs, circle graphs, line plots, tables, and other ways of representing data.",
+    picture: "img/jenniferl.jpg",
+    instructor: "Jennifer Ledford",
+    email: "jennifer@laureladulted.org",
+    phone: "(606) 528-0379"
     },
     
-    {name: coursesList[3],
-    days: daysList[0],
-    time: timesList[1],
-    syllabus: linksList[1].cornettGra,
-    zoom: linksList[0].clay,
-    description: description.grammar,
-    instructor: "Ryan Cornett",
-    email: "ryan@laureladulted.org",
-    phone: phoneNumbers.clay
+    {name: "Geometry",
+    days: "Thursday",
+    time: "1:00-2:30",
+    syllabus: "https://docs.google.com/document/d/1EEQ88RKRlLGmUWX7I-w4Wm7YQC5TJVCAS_ECEC-n9Zc",
+    zoom: "https://us02web.zoom.us/j/6447967863?pwd=aldCSW50bGlPVzhHMi9oczR4QUJUQT09",
+    description: "In this class, you'll work on area, perimeter, surface area, and volume formulas. Plus, you'll look at data analysis concepts like mean, median, mode, range, and probability.",
+    picture: "img/rachaelc.jpg",
+    instructor: "Rachael Chestnut",
+    email: "rachael@laureladulted.org",
+    phone: "(606) 878-9134"
     },
-
-    {name: coursesList[4],
-    days: `${daysList[0]} & ${daysList[2]}`,
-    time: timesList[3],
-    syllabus: linksList[1].martin,
-    zoom: linksList[0].knox,
-    description: description.rla,
-    instructor: "Della Martin",
-    email: "della.martin@knox.kyschools.us",
-    phone: phoneNumbers.knox
-    },
-
-    {name: coursesList[4],
-    days: `${daysList[0]} & ${daysList[1]}`,
-    time: timesList[4],
-    syllabus: linksList[1].gaines,
-    zoom: linksList[0].laurel,
-    description: description.rlaSS,
-    instructor: "Robert Gaines",
-    email: "robert@laureladulted.org",
-    phone: phoneNumbers.london
-    },
-
-    {name: coursesList[5],
-    days: daysList[3],
-    time: timesList[2],
-    syllabus: linksList[1].jones,
-    zoom: linksList[0].whitley,
-    description: description.reading,
-    instructor: "Kayla Jones",
-    email: "kayla.jones@whitley.kyschools.us",
-    phone: phoneNumbers.whitley
-    },
-
-    {name: coursesList[6],
-    days: `${daysList[0]} & ${daysList[1]}`,
-    time: timesList[4],
-    syllabus: linksList[1].gaines,
-    zoom: linksList[0].laurel,
-    description: description.rlaSS,
-    instructor: "Robert Gaines",
-    email: "robert@laureladulted.org",
-    phone: phoneNumbers.london
-    },
-
-    {name: coursesList[7],
-    days: `${daysList[1]} & ${daysList[3]}`,
-    time: timesList[3],
-    syllabus: linksList[1].newquist,
-    zoom: linksList[0].knox,
-    description: description.science,
-    instructor: "Terry Newquist",
-    email: "terry@laureladulted.org",
-    phone: phoneNumbers.knox
-    },
-
-    {name: coursesList[6],
-    days: `${daysList[2]} & ${daysList[3]}`,
-    time: timesList[1],
-    syllabus: linksList[1].peters,
-    zoom: linksList[0].knox,
-    description: description.socialStudies,
-    instructor: "Austin Peters",
-    email: "austin.peters@knox.kyschools.us",
-    phone: phoneNumbers.knox
-    },
-
-    {name: coursesList[6],
-    days: daysList[2],
-    time: timesList[5],
-    syllabus: linksList[1].gibbins,
-    zoom: linksList[0].whitley,
-    description: description.rlaSS,
-    instructor: "Zac Gibbins",
-    email: "zac.gibbins@whitley.kyschools.us",
-    phone: phoneNumbers.whitley
+    
+    {name: "Algebra",
+    days: "Thursday",
+    time: "10:30-12:00",
+    syllabus: "https://docs.google.com/document/u/0/d/1EEQ88RKRlLGmUWX7I-w4Wm7YQC5TJVCAS_ECEC-n9Zc",
+    zoom: "https://us02web.zoom.us/j/6447967863?pwd=aldCSW50bGlPVzhHMi9oczR4QUJUQT09",
+    description: "In this class, you'll work on expressions, equations, inequalities, functions, and graphs.",
+    picture: "img/erinm.jpg",
+    instructor: "Erin Moses",
+    email: "erin@laureladulted.org",
+    phone: "(606) 528-0379"
     }
 ];
 
+let englishCourses = [
+    {name: "Grammar & Writing",
+    days: "Monday",
+    time: "10:30-12:00",
+    syllabus: "https://docs.google.com/document/d/1VxzMq3oCd4XtE9U8i78DGcO6zYqIGMnS/edit?usp=share_link&ouid=115847489402002050925&rtpof=true&sd=true",
+    zoom: "https://us02web.zoom.us/j/3619478526?pwd=WkJZUUUzdUx3dVp5RVdoV1h0amF0QT09",
+    description: "In this class, you'll learn the science of how Standard American English works. Better command of the language means better command of your audience.",
+    picture: "img/ryanc.jpg",
+    instructor: "Ryan Cornett",
+    email: "ryan@laureladulted.org",
+    phone: "(606) 599-1230"
+    },
+    
+    {name: "Language Arts/Social Studies",
+    days: "Monday",
+    time: "4:00-5:30",
+    syllabus: "https://docs.google.com/document/d/1J5qYvq_pgTa2vjvzQ-BEH6NmGWeS_QYW",
+    zoom: "https://us02web.zoom.us/j/6447967863?pwd=aldCSW50bGlPVzhHMi9oczR4QUJUQT09",
+    description: "By the end of this course, participants will understand social studies concepts, apply social studies concepts, read graphs and charts displaying social studies data, use reasoning, and interpret social studies information",
+    picture: "img/student.jpg",
+    instructor: "Robert Gaines",
+    email: "robert@laureladulted.org",
+    phone: "(606) 878-9134"
+    },
+    
+    {name: "Reading",
+    days: "Thursday",
+    time: "1:00-2:30",
+    syllabus: "https://docs.google.com/document/d/1OBO3t2L06DDkJ9ey9yHB_Bmg1QUpboUX/edit#heading=h.gjdgxs",
+    zoom: "https://us06web.zoom.us/j/89007426822",
+    description: "In this class, students will be introduced to various reading foundations and strategy skills to help them learn to read and improve their reading skills.",
+    picture: "img/kaylaj.jpg",
+    instructor: "Kaya Jones",
+    email: "kayla.jones@whitley.kyschools.us",
+    phone: "(606) 549-1989"
+    }
+];
 
-function insert() {
-    for (let a = 0; a < courseNameElement.length; a++) {
-        courseNameElement[a].textContent = courses[a].name;
+let otherCourses = [
+    {name: "Language Arts/Social Studies",
+    days: "Monday",
+    time: "4:00-5:30",
+    syllabus: "https://docs.google.com/document/d/1J5qYvq_pgTa2vjvzQ-BEH6NmGWeS_QYW",
+    zoom: "https://us02web.zoom.us/j/6447967863?pwd=aldCSW50bGlPVzhHMi9oczR4QUJUQT09",
+    description: "By the end of this course, participants will understand social studies concepts, apply social studies concepts, read graphs and charts displaying social studies data, use reasoning, and interpret social studies information",
+    picture: "img/student.jpg",
+    instructor: "Robert Gaines",
+    email: "robert@laureladulted.org",
+    phone: "(606) 878-9134"
+    },
+
+    {name: "Facilitated Lab",
+    days: "Tuesday",
+    time: "10:00-12:00",
+    syllabus: "google.com",
+    zoom: "https://us02web.zoom.us/j/86233937397?pwd=bVhnSWF0NmF6SFRyYmhOSlE3bXlXdz09",
+    description: "Facilitated Lab is a...",
+    picture: "img/student.jpg",
+    instructor: "Della Martin",
+    email: "della.martin@knox.kyschools.us",
+    phone: "(606) 545-6032"
+    },
+
+    {name: "Social Studies",
+    days: "Wednesday & Thursday",
+    time: "10:30-12:00",
+    syllabus: "https://docs.google.com/document/d/1Ex8_nomM_ex3dmGJarMkSPK9HuBPKCXr",
+    zoom: "https://us02web.zoom.us/j/89829217809?pwd=T1VSQ1A3S2wyTnRKZ1BHQUVDd0RRUT09",
+    description: "Students will exit this course with necessary skills for passing the GED Social Studies Test. Topics include civics and government, United States history, economics, world geography, and social studies practices.",
+    picture: "img/student.jpg",
+    instructor: "Austin Peters",
+    email: "austin.peters@knox.kyschools.us",
+    phone: "(606) 545-6032"
+    },
+
+    {name: "Open Lab",
+    days: "Thursday",
+    time: "2:00-4:00",
+    syllabus: "google.com",
+    zoom: "https://us06web.zoom.us/j/83660895039?pwd=NCtBUWNBUTJlazBUeHN0OW9jT2RBUT09",
+    description: "Open Lab is a...",
+    picture: "img/student.jpg",
+    instructor: "Zac Gibbins",
+    email: "zac.gibbins@whitley.kyschools.us",
+    phone: "(606) 549-1989"
     }
-    for (let b = 0; b < daysElement.length; b++) {
-        daysElement[b].textContent = courses[b].days;
-    }
-    for (let c = 0; c < timesElement.length; c++) {
-        timesElement[c].textContent = courses[c].time;
-    }
-    for (let d = 0; d < syllabusElement.length; d++) {
-        syllabusElement[d].innerHTML = `<a href=" ${courses[d].syllabus}">Syllabus</a>`;
-    }
-    for (let e = 0; e < zoomElement.length; e++) {
-        zoomElement[e].innerHTML = `<a href=" ${courses[e].zoom}">Zoom Meeting Room</a>`;
-    }
-    for (let f = 0; f < descriptionElement.length; f++) {
-        descriptionElement[f].textContent = courses[f].description;
-    }
-    for (let g = 0; g < instructorElement.length; g++) {
-        instructorElement[g].textContent = courses[g].instructor;
-    }
-    for (let h = 0; h < emailAddressElement.length; h++) {
-        emailAddressElement[h].textContent = courses[h].email;
-    }
-    for (let i = 0; i < phoneNumberElement.length; i++) {
-        phoneNumberElement[i].textContent = courses[i].phone;
-    }
-};
-export {insert as default};
+];
+
+export { mathCourses, englishCourses, otherCourses };
--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 'use strict';
-import populateElements from './courses.js'
-populateElements();
+import { mathCourses, englishCourses, otherCourses } from './courses.js'
 
 const topButton = document.getElementById('to-top');
 
@@ -13,6 +12,122 @@ function displayTopButton() {
 }
 
 document.addEventListener('scroll', displayTopButton);
+
+let mathContainer = document.querySelector('.math-container');
+let rlaContainer = document.querySelector('.rla-container');
+let otherContainer = document.querySelector('.other-container');
+let waysContainer = document.querySelector('.ways-container');
+
+let courseDiv = document.createElement("div");
+courseDiv.setAttribute("class", "course");
+
+function generateSubjectDiv(subject, container) {
+    courseDiv.innerHTML = "";
+    for (let i = 0; i < subject.length; i++) {
+        let courseName = document.createElement("h3");
+        courseName.setAttribute("class", "course-name");
+        courseName.innerText = subject[i].name;
+        courseDiv.appendChild(courseName);
+        let infoDiv = document.createElement('div');
+        infoDiv.setAttribute('class', 'course-info');
+        courseDiv.appendChild(infoDiv);
+        let courseDays = document.createElement('h4');
+        courseDays.setAttribute('class', 'days');
+        courseDays.innerText = subject[i].days;
+        infoDiv.appendChild(courseDays);
+        let courseTime = document.createElement('h4');
+        courseTime.setAttribute('class', 'time');
+        courseTime.innerText = subject[i].time;
+        infoDiv.appendChild(courseTime);
+        let linksDiv = document.createElement('div');
+        linksDiv.setAttribute('class', 'course-links');
+        courseDiv.appendChild(linksDiv);
+        let syllabusLink = document.createElement('p');
+        syllabusLink.setAttribute('class', 'syllabus');
+        syllabusLink.innerHTML = `<a href = "${subject[i].syllabus}">Course Syllabus</a>`;
+        linksDiv.appendChild(syllabusLink);
+        let zoomLink = document.createElement('p');
+        zoomLink.setAttribute('class', 'zoom');
+        zoomLink.innerHTML = `<a href = "${subject[i].zoom}">Zoom Meeting Link</a>`;
+        linksDiv.appendChild(zoomLink);
+        let description = document.createElement('span');
+        description.setAttribute('class', 'description');
+        description.textContent = subject[i].description;
+        courseDiv.appendChild(description);
+        let profileDiv = document.createElement('div');
+        profileDiv.setAttribute('class', 'profile');
+        courseDiv.appendChild(profileDiv);
+        let picture = document.createElement('img');
+        picture.setAttribute('src', subject[i].picture);
+        picture.setAttribute('class', 'picture');
+        picture.setAttribute('alt', subject[i].instructor);
+        profileDiv.appendChild(picture);
+        let instructor = document.createElement('h4');
+        instructor.setAttribute('class', 'instructor');
+        instructor.textContent = subject[i].instructor;
+        profileDiv.appendChild(instructor);
+        let email = document.createElement('p');
+        email.setAttribute('class', 'email');
+        email.textContent = subject[i].email;
+        profileDiv.appendChild(email);
+        let phone = document.createElement('p');
+        phone.setAttribute('class', 'phone');
+        phone.textContent = subject[i].phone;
+        profileDiv.appendChild(phone);
+        container.appendChild(courseDiv);
+}};
+
+const MATH_MODAL = document.getElementById('math-modal');
+const MATH_BUTTON = document.getElementById('open-math');
+MATH_BUTTON.addEventListener('click', () => {
+generateSubjectDiv(mathCourses, mathContainer);
+MATH_MODAL.showModal();
+console.log('click');
+});
+
+let closeMathButton = document.querySelectorAll('.close-math');
+closeMathButton.forEach((buttons) => {
+  buttons.addEventListener('click', () => {
+    MATH_MODAL.close();
+  });
+});
+
+const RLA_MODAL = document.getElementById('rla-modal');
+const RLA_BUTTON = document.getElementById('open-rla');
+RLA_BUTTON.addEventListener('click', () => {
+generateSubjectDiv(englishCourses, rlaContainer);
+RLA_MODAL.showModal();
+console.log('click');
+});
+
+let closeRlaButton = document.querySelectorAll('.close-rla');
+closeRlaButton.forEach((buttons) => {
+  buttons.addEventListener('click', () => {
+    RLA_MODAL.close();
+  });
+});
+
+const OTHER_MODAL = document.getElementById('other-modal');
+const OTHER_BUTTON = document.getElementById('open-other');
+OTHER_BUTTON.addEventListener('click', () => {
+generateSubjectDiv(otherCourses, otherContainer);
+OTHER_MODAL.showModal();
+console.log('click');
+});
+
+let closeOtherButton = document.querySelectorAll('.close-other');
+closeOtherButton.forEach((buttons) => {
+  buttons.addEventListener('click', () => {
+    OTHER_MODAL.close();
+    WAYS_MODAL.close();
+  });
+});
+
+const WAYS_MODAL = document.getElementById('ways-modal');
+const WAYS_BUTTON = document.getElementById('open-ways');
+WAYS_BUTTON.addEventListener('click', () => {
+  WAYS_MODAL.showModal();
+});
 
 let currentYear = new Date().getFullYear();
 let credit = document.getElementById('bottom-credit');
