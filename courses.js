7bccbbf34f8750e7e15a4066477a077745075c43
Ryan Cornett <ryandcornett@gmail.com>
2023-04-28 10:55:16 -0400

    Updated courses.

--- courses.js
+++ courses.js
@@ -8,7 +8,7 @@ const COURSE01 = new Course("Number Sense", "Monday & Tuesday", "9:00-10:00", "h
 
 const COURSE02 = new Course("Algebra", "Tuesday", "5:00-6:30", "https://docs.google.com/document/d/1WYkF4sSxJ9EgesBxfhVLGHaJ3iD9ZdIB/edit?usp=share_link&ouid=104017723111487459394&rtpof=true&sd=true", "https://us02web.zoom.us/j/3619478526?pwd=WkJZUUUzdUx3dVp5RVdoV1h0amF0QT09", "In this class, you'll work on expressions, equations, inequalities, functions, and graphs.", "img/rodneyl.webp", "Rodney Lipps", "rodney@laureladulted.org", "(606) 599-1230");
 
-const COURSE03 = new Course("Number Sense", "Wednesday", "10:30-12:00", "https://docs.google.com/document/d/1sNDHvD5xIXkQ_tqmWdguvl1L9KnBTL5y", "https://us02web.zoom.us/j/4974845563?pwd=Q1ErU2gzaW9oNiszYmkyZXRVajRSUT09", "In this class, you'll sharpen your skills with operations, fractions, decimals, percents, and word problems. You'll also work with reading bar graphs, circle graphs, line plots, tables, and other ways of representing data.", "img/jenniferl.webp", "Jennifer Ledford", "jennifer@laureladulted.org", "(606) 528-0379");
+const COURSE03 = new Course("Math Foundations", "Wednesday", "10:30-12:00", "https://docs.google.com/document/d/12AWYyoIoSb8FPdp5ccw2wBdIlJfXGUzW", "https://us02web.zoom.us/j/4974845563?pwd=Q1ErU2gzaW9oNiszYmkyZXRVajRSUT09", "This course will introduce learners to the concepts and skills necessary to progress to the GED mathematics exam or improve applied mathematics skills for the workforce or post-secondary education.", "img/jenniferl.webp", "Jennifer Ledford", "jennifer@laureladulted.org", "(606) 528-0379");
 
 const COURSE04 = new Course("Geometry", "Thursday", "1:00-2:30", "https://docs.google.com/document/d/1EEQ88RKRlLGmUWX7I-w4Wm7YQC5TJVCAS_ECEC-n9Zc", "https://us02web.zoom.us/j/6447967863?pwd=aldCSW50bGlPVzhHMi9oczR4QUJUQT09", "In this class, you'll work on area, perimeter, surface area, and volume formulas. Plus, you'll look at data analysis concepts like mean, median, mode, range, and probability.", "img/rachaelc.webp", "Rachael Chestnut", "rachael@laureladulted.org", "(606) 878-9134");
 
@@ -16,15 +16,15 @@ const COURSE05 = new Course("Algebra", "Thursday", "10:30-12:00", "https://docs.
 
 let mathCourses = [COURSE01, COURSE02, COURSE03, COURSE04, COURSE05];
 
-const COURSE06 = new Course("Grammar & Writing", "Monday", "10:30-12:00", "https://docs.google.com/document/d/1VxzMq3oCd4XtE9U8i78DGcO6zYqIGMnS/edit?usp=share_link&ouid=115847489402002050925&rtpof=true&sd=true", "https://us02web.zoom.us/j/3619478526?pwd=WkJZUUUzdUx3dVp5RVdoV1h0amF0QT09", "In this class, you'll learn the science of how Standard American English works. Better command of the language means better command of your audience.", "img/ryanc.webp", "Ryan Cornett", "ryan@laureladulted.org", "(606) 599-1230");
+const COURSE06 = new Course("Grammar & Writing", "Monday", "10:30-12:00", "https://drive.google.com/file/d/1V_ubuj5SD_PdzRQ7inrvUWrtYKbEG1n-", "https://us02web.zoom.us/j/3619478526?pwd=WkJZUUUzdUx3dVp5RVdoV1h0amF0QT09", "In this class, you'll learn how to write essays and the science of how Standard American English works. Better command of the language means better command of your audience.", "img/ryanc.webp", "Ryan Cornett", "ryan@laureladulted.org", "(606) 599-1230");
 
-const COURSE07 = new Course("Language Arts/Social Studies", "Monday", "4:00-5:30", "https://docs.google.com/document/d/1J5qYvq_pgTa2vjvzQ-BEH6NmGWeS_QYW", "https://us02web.zoom.us/j/6447967863?pwd=aldCSW50bGlPVzhHMi9oczR4QUJUQT09", "By the end of this course, participants will understand social studies concepts, apply social studies concepts, read graphs and charts displaying social studies data, use reasoning, and interpret social studies information", "img/person_icon.webp", "Robert Gaines", "robert@laureladulted.org", "(606) 878-9134");
+// const COURSE07 = new Course("Language Arts/Social Studies", "Monday", "4:00-5:30", "https://docs.google.com/document/d/1J5qYvq_pgTa2vjvzQ-BEH6NmGWeS_QYW", "https://us02web.zoom.us/j/6447967863?pwd=aldCSW50bGlPVzhHMi9oczR4QUJUQT09", "By the end of this course, participants will understand social studies concepts, apply social studies concepts, read graphs and charts displaying social studies data, use reasoning, and interpret social studies information", "img/person_icon.webp", "Robert Gaines", "robert@laureladulted.org", "(606) 878-9134");
 
-const COURSE08 = new Course("Reading", "Thursday", "1:00-2:30", "https://docs.google.com/document/d/1OBO3t2L06DDkJ9ey9yHB_Bmg1QUpboUX/edit#heading=h.gjdgxs", "https://us06web.zoom.us/j/89007426822", "In this class, students will be introduced to various reading foundations and strategy skills to help them learn to read and improve their reading skills.", "img/kaylaj.webp", "Kaya Jones", "kayla.jones@whitley.kyschools.us", "(606) 549-1989");
+const COURSE08 = new Course("Phonics Fundamentals", "Thursday", "1:00-2:30", "https://drive.google.com/file/d/1ynjejcMlSgcoryKYJt0Dsrb7hsFIuKiq", "https://us06web.zoom.us/j/89007426822", "This course will introduce learners to the concepts and skills necessary to learn/improve reading skills. The course is taught using lesson plans based on the College and Career Readiness Standards.", "img/kaylaj.webp", "Kaya Jones", "kayla.jones@whitley.kyschools.us", "(606) 549-1989");
 
-let englishCourses = [COURSE06, COURSE07, COURSE08]
+let englishCourses = [COURSE06, COURSE08]
 
-const COURSE09 = new Course("Language Arts/Social Studies", "Monday", "4:00-5:30", "https://docs.google.com/document/d/1J5qYvq_pgTa2vjvzQ-BEH6NmGWeS_QYW", "https://us02web.zoom.us/j/6447967863?pwd=aldCSW50bGlPVzhHMi9oczR4QUJUQT09", "By the end of this course, participants will understand social studies concepts, apply social studies concepts, read graphs and charts displaying social studies data, use reasoning, and interpret social studies information", "img/person_icon.webp", "Robert Gaines", "robert@laureladulted.org", "(606) 878-9134");
+// const COURSE09 = new Course("Language Arts/Social Studies", "Monday", "4:00-5:30", "https://docs.google.com/document/d/1J5qYvq_pgTa2vjvzQ-BEH6NmGWeS_QYW", "https://us02web.zoom.us/j/6447967863?pwd=aldCSW50bGlPVzhHMi9oczR4QUJUQT09", "By the end of this course, participants will understand social studies concepts, apply social studies concepts, read graphs and charts displaying social studies data, use reasoning, and interpret social studies information", "img/person_icon.webp", "Robert Gaines", "robert@laureladulted.org", "(606) 878-9134");
 
 const COURSE10 = new Course("Facilitated Lab", "Tuesday", "10:00-12:00", "google.com", "https://us02web.zoom.us/j/86233937397?pwd=bVhnSWF0NmF6SFRyYmhOSlE3bXlXdz09", "Facilitated Lab is a...", "img/person_icon.webp", "Della Martin", "della.martin@knox.kyschools.us", "(606) 545-6032");
 
@@ -32,7 +32,7 @@ const COURSE11 = new Course("Social Studies", "Wednesday & Thursday", "10:30-12:
 
 const GIBBINS_OPEN_LAB = new Course("Open Lab", "Thursday", "2:00-4:00", "https://docs.google.com/document/d/10uEX9Xf5Q0MitRv7AOZkg2TrJXN-YPirOFLV5Y4nOFo", "https://us06web.zoom.us/j/83660895039?pwd=NCtBUWNBUTJlazBUeHN0OW9jT2RBUT09", "Do you have a math skill you want extra practice with? Is there a science idea you want to explore? Want a refresher on a social studies concept? Maybe even a quick boot camp with reading and writing strategies? Any topic, any subject, your choice. Just let your instructor know what you need when you join.", "img/zacg.webp", "Zac Gibbins", "zac.gibbins@whitley.kyschools.us", "(606) 549-1989");
 
-let otherCourses = [COURSE09, COURSE10, COURSE11, GIBBINS_OPEN_LAB]
+let otherCourses = [COURSE10, COURSE11, GIBBINS_OPEN_LAB]
 
 function generateSubjectDiv(subject, container, parent) {
 parent.innerHTML = "";
