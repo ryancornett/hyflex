853828572779e6f3667fa86763df0d5942428b18
Ryan Cornett <ryandcornett@gmail.com>
2023-09-05 08:05:58 -0400

    Remove defunct course and notes

--- scripts/courses.js
+++ scripts/courses.js
@@ -7,7 +7,7 @@ class Course {
 const NOTE_START = "<p style='color:red; font-weight:200; font-size: 0.8rem'><b>";
 const NOTE_END = "</b></p>"
 
-const SIZEMORE_NUMBER_SENSE = new Course("Number Sense", "Monday & Tuesday", "9:00-10:00", "https://docs.google.com/document/d/1lBnj1anz5cOa1L5bbV-CUbs2pysiH7Ys/edit#heading=h.gjdgxs", "https://us02web.zoom.us/j/6447967863?pwd=aldCSW50bGlPVzhHMi9oczR4QUJUQT09", `In this class, you'll sharpen your skills with operations, fractions, decimals, percents, and word problems. You'll also work with reading bar graphs, circle graphs, line plots, tables, and other ways of representing data.${NOTE_START}No class 9/4${NOTE_END}`, "img/marks.webp", "Mark Sizemore", "mark@laureladulted.org", "(606) 878-9134");
+const SIZEMORE_NUMBER_SENSE = new Course("Number Sense", "Monday & Tuesday", "9:00-10:00", "https://docs.google.com/document/d/1lBnj1anz5cOa1L5bbV-CUbs2pysiH7Ys/edit#heading=h.gjdgxs", "https://us02web.zoom.us/j/6447967863?pwd=aldCSW50bGlPVzhHMi9oczR4QUJUQT09", `In this class, you'll sharpen your skills with operations, fractions, decimals, percents, and word problems. You'll also work with reading bar graphs, circle graphs, line plots, tables, and other ways of representing data.`, "img/marks.webp", "Mark Sizemore", "mark@laureladulted.org", "(606) 878-9134");
 
 const LIPPS_ALGEBRA = new Course("Algebra", "Tuesday", "5:00-6:30", "https://docs.google.com/document/d/1WYkF4sSxJ9EgesBxfhVLGHaJ3iD9ZdIB/edit?usp=share_link&ouid=104017723111487459394&rtpof=true&sd=true", "https://us02web.zoom.us/j/3619478526?pwd=WkJZUUUzdUx3dVp5RVdoV1h0amF0QT09", `In this class, you'll work on expressions, equations, inequalities, functions, and graphs.`, "img/rodneyl.webp", "Rodney Lipps", "rodney@laureladulted.org", "(606) 599-1230");
 
@@ -19,19 +19,17 @@ const MOSES_ALGEBRA = new Course("Algebra", "Thursday", "10:30-12:00", "https://
 
 let mathCourses = [SIZEMORE_NUMBER_SENSE, LIPPS_ALGEBRA, LEDFORD_MATH_FOUNDATIONS, MOSES_ALGEBRA, CHESTNUT_GEOMETRY];
 
-const CORNETT_GRAMMAR = new Course("Grammar & Writing", "Monday", "10:30-12:00", "https://docs.google.com/document/d/1cFHIMcdR3V1Nz8zG_VCGzIbAWj6NQDhp", "https://us02web.zoom.us/j/3619478526?pwd=WkJZUUUzdUx3dVp5RVdoV1h0amF0QT09", `In this class, you'll learn how to write essays and the science of how Standard American English works. Better command of the language means better command of your audience.${NOTE_START}No class 9/4${NOTE_END}`, "img/ryanc.webp", "Ryan Cornett", "ryan@laureladulted.org", "(606) 599-1230");
+const CORNETT_GRAMMAR = new Course("Grammar & Writing", "Monday", "10:30-12:00", "https://docs.google.com/document/d/1cFHIMcdR3V1Nz8zG_VCGzIbAWj6NQDhp", "https://us02web.zoom.us/j/3619478526?pwd=WkJZUUUzdUx3dVp5RVdoV1h0amF0QT09", `In this class, you'll learn how to write essays and the science of how Standard American English works. Better command of the language means better command of your audience.`, "img/ryanc.webp", "Ryan Cornett", "ryan@laureladulted.org", "(606) 599-1230");
 
 const JONES_PHONICS = new Course("Phonics Fundamentals", "Thursday", "1:00-2:30", "https://drive.google.com/file/d/1ynjejcMlSgcoryKYJt0Dsrb7hsFIuKiq", "https://us06web.zoom.us/j/89007426822", `This course will introduce learners to the concepts and skills necessary to learn/improve reading skills. The course is taught using lesson plans based on the College and Career Readiness Standards.`, "img/kaylaj.webp", "Kayla Jones", "kayla.jones@whitley.kyschools.us", "(606) 549-1989");
 
 let englishCourses = [CORNETT_GRAMMAR, JONES_PHONICS]
 
-const MARTIN_LAB = new Course("Facilitated Lab", "Tuesday", "10:00-12:00", "https://drive.google.com/file/d/1snMiuVuAVjIbC3TIE_bYlpJbifsSvSpG", "https://us02web.zoom.us/j/82946614012?pwd=OWxVOFRXR2ZaZVRhRTFlT2pJSkVjZz09", "Tutoring in all four subject areas (language arts, math, science, social studies), catering to what skills you need to build and improve to pass the GED.", "img/dellam.jpg", "Della Martin", "della.martin@knox.kyschools.us", "(606) 545-6032");
-
 const PETERS_SOCIAL_STUDIES = new Course("Social Studies", "Wednesday", "10:30-12:00", "https://docs.google.com/document/d/1Ex8_nomM_ex3dmGJarMkSPK9HuBPKCXr", "https://us02web.zoom.us/j/89829217809?pwd=T1VSQ1A3S2wyTnRKZ1BHQUVDd0RRUT09", "Students will exit this course with necessary skills for passing the GED Social Studies Test. Topics include civics and government, United States history, economics, world geography, and social studies practices.", "img/austinp.jpg", "Austin Peters", "austin.peters@knox.kyschools.us", "(606) 545-6032");
 
 const GIBBINS_LAB = new Course("Open Lab", "Thursday", "2:00-4:00", "https://docs.google.com/document/d/10uEX9Xf5Q0MitRv7AOZkg2TrJXN-YPirOFLV5Y4nOFo", "https://us06web.zoom.us/j/83660895039?pwd=NCtBUWNBUTJlazBUeHN0OW9jT2RBUT09", `Do you have a math skill you want extra practice with? Is there a science idea you want to explore? Want a refresher on a social studies concept? Maybe even a quick boot camp with reading and writing strategies? Any topic, any subject, your choice. Just let your instructor know what you need when you join.`, "img/zacg.webp", "Zac Gibbins", "zac.gibbins@whitley.kyschools.us", "(606) 549-1989");
 
-let otherCourses = [MARTIN_LAB, PETERS_SOCIAL_STUDIES, GIBBINS_LAB]
+let otherCourses = [PETERS_SOCIAL_STUDIES, GIBBINS_LAB]
 
 function generateSubjectDiv(subject, container, parent) {
 parent.innerHTML = "";
