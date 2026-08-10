d1d8f406993468dae7d8a7137065022696d09a31
Ryan Cornett <ryandcornett@gmail.com>
2026-08-10 09:23:57 -0400

    remove old course notes

--- scripts/courses.js
+++ scripts/courses.js
@@ -110,11 +110,11 @@ const mathCourses = [
 
     new Course(Tiers.I, Subjects.M, "#01", "Monday", "10:30-11:15", FM_Folder, CORBIN_ZOOM, `${IM_Overview}`, "img/jenniferl.webp", "Jennifer Ledford", "jennifer@laureladulted.org", CORBIN_PHONE, "a3"),
 
-    new Course(Tiers.F, Subjects.M, "- Ready to Test", "Monday", "11:15-12:00", FM_Folder, LONDON_ZOOM, `The First Five: math without a calculator.${NOTE_START}Beginning 8/10/2026${NOTE_END}`, "img/jenniferl.webp", "Jennifer Ledford", "jennifer@laureladulted.org", CORBIN_PHONE, "a4"),
+    new Course(Tiers.F, Subjects.M, "- Ready to Test", "Monday", "11:15-12:00", FM_Folder, LONDON_ZOOM, `The First Five: math without a calculator.`, "img/jenniferl.webp", "Jennifer Ledford", "jennifer@laureladulted.org", CORBIN_PHONE, "a4"),
 
     new Course(Tiers.I, Subjects.M, "#02", "Tuesday", "10:30-11:15", IM_Folder, CORBIN_ZOOM, `${IM_Overview}`, "img/erinm.webp", "Erin Moses", "erin@laureladulted.org", CORBIN_PHONE, "b3"),
     
-    new Course(Tiers.I, Subjects.M, "- Ready to Test", "Tuesday", "11:15-12:00", IM_Folder, CORBIN_ZOOM, `Calculator Confidence. **NOTE: attendees can use a borrowed calculator (TI-30XS), or can access one via Android app or <a href="https://gedonline.org/ged2015/calculator/calculator.html" target="_blank" style="font-weight:bold;">here</a>.${NOTE_START}Beginning 8/11/2026${NOTE_END}`, "img/erinm.webp", "Erin Moses", "erin@laureladulted.org", CORBIN_PHONE, "b4"),
+    new Course(Tiers.I, Subjects.M, "- Ready to Test", "Tuesday", "11:15-12:00", IM_Folder, CORBIN_ZOOM, `Calculator Confidence. **NOTE: attendees can use a borrowed calculator (TI-30XS), or can access one via Android app or <a href="https://gedonline.org/ged2015/calculator/calculator.html" target="_blank" style="font-weight:bold;">here</a>.`, "img/erinm.webp", "Erin Moses", "erin@laureladulted.org", CORBIN_PHONE, "b4"),
 
     new Course(Tiers.I, Subjects.M, "#03", "Tuesday", "5:30-6:15", IM_Folder, CLAY_ZOOM, `${IM_Overview}`, "img/rodneyl.webp", "Rodney Lipps", "rodney@laureladulted.org", CLAY_PHONE, "b11"),
 
@@ -124,7 +124,7 @@ const mathCourses = [
 
     new Course(Tiers.F, Subjects.M, "#02", "Wednesday", "4:00-4:45", FM_Overview, LONDON_ZOOM, `${FM_Overview}`, "img/marks.webp", "Mark Sizemore", "mark@laureladulted.org", LONDON_PHONE, "c9"),
 
-    new Course(Tiers.I, Subjects.M, "- Ready to Test", "Thursday", "1:00-1:45", IM_Overview, LONDON_ZOOM, `Last-Minute Math: a live test-prep session.${NOTE_START}Beginning 8/13/2026${NOTE_END}`, "img/rachaelc_v2.webp", "Rachael Chestnut", "rachael@laureladulted.org", LONDON_PHONE, "d5"),
+    new Course(Tiers.I, Subjects.M, "- Ready to Test", "Thursday", "1:00-1:45", IM_Overview, LONDON_ZOOM, `Last-Minute Math: a live test-prep session.`, "img/rachaelc_v2.webp", "Rachael Chestnut", "rachael@laureladulted.org", LONDON_PHONE, "d5"),
     
     new Course(Tiers.I, Subjects.M, "#05", "Thursday", "2:30-3:15", IM_Overview, LONDON_ZOOM, `${IM_Overview}`, "img/marks.webp", "Mark Sizemore", "mark@laureladulted.org", LONDON_PHONE, "d7"),
 ];
